8d669559b3d8f3dc4e0d169cabce8452a400f4e6
CANCEAL <alfred.cancejo@oocl.com>
2020-10-19 15:39:21 +0800

    [Word Frequency] [Alfred] Refactor: renamed s to combineWordInfo

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -39,8 +39,8 @@ public class WordFrequencyGame {
 
                 StringJoiner joiner = new StringJoiner("\n");
                 for (WordInfo wordInfo : wordInfoList) {
-                    String s = wordInfo.getValue() + " " +wordInfo.getWordCount();
-                    joiner.add(s);
+                    String combineWordInfo = wordInfo.getValue() + " " +wordInfo.getWordCount();
+                    joiner.add(combineWordInfo);
                 }
                 return joiner.toString();
             } catch (Exception e) {
