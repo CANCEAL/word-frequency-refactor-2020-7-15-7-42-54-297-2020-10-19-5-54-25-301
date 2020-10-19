4bb80eee5785a18a1acea76ab339f3d4bf8e3331
CANCEAL <alfred.cancejo@oocl.com>
2020-10-19 15:53:40 +0800

    [Word Frequency] [Alfred] Refactor: updated getWordFrequency method

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -1,8 +1,4 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.StringJoiner;
+import java.util.*;
 
 public class WordFrequencyGame {
     private static final String WHITE_SPACE = "\\s+";
@@ -12,12 +8,9 @@ public class WordFrequencyGame {
         if (sentence.split(WHITE_SPACE).length==1) {
             return sentence + " 1";
         } else {
-
             try {
                 List<WordInfo> wordInfoList = getWordFrequency(sentence);
-
                 wordInfoList.sort((firstWord, secondWord) -> secondWord.getWordCount() - firstWord.getWordCount());
-
                 StringJoiner joiner = getStringJoiner(wordInfoList);
                 return joiner.toString();
             } catch (Exception e) {
@@ -36,45 +29,13 @@ public class WordFrequencyGame {
     }
 
     private List<WordInfo> getWordFrequency(String sentence) {
-        String[] words = sentence.split(WHITE_SPACE);
+        List<String> words = Arrays.asList(sentence.split(WHITE_SPACE));
 
         List<WordInfo> wordInfoList = new ArrayList<>();
-        for (String word : words) {
-            WordInfo wordInfo = new WordInfo(word, 1);
-            wordInfoList.add(wordInfo);
+        for (String word : new HashSet<>(words)) {
+            int count = Collections.frequency(words, word);
+            wordInfoList.add(new WordInfo(word, count));
         }
-
-        //get the map for the next step of sizing the same word
-        Map<String, List<WordInfo>> wordInfoMap = getListMap(wordInfoList);
-
-        List<WordInfo> distinctWordInfos = new ArrayList<>();
-        for (Map.Entry<String, List<WordInfo>> entry : wordInfoMap.entrySet()){
-            WordInfo wordInfo = new WordInfo(entry.getKey(), entry.getValue().size());
-            distinctWordInfos.add(wordInfo);
-        }
-        return wordInfoList = distinctWordInfos;
+        return wordInfoList;
     }
-
-
-
-    private Map<String,List<WordInfo>> getListMap(List<WordInfo> wordInfoList) {
-        Map<String, List<WordInfo>> map = new HashMap<>();
-        for (WordInfo wordInfo : wordInfoList){
-//       map.computeIfAbsent(input.getValue(), k -> new ArrayList<>()).add(input);
-            if (!map.containsKey(wordInfo.getValue())){
-                ArrayList arr = new ArrayList<>();
-                arr.add(wordInfo);
-                map.put(wordInfo.getValue(), arr);
-            }
-
-            else {
-                map.get(wordInfo.getValue()).add(wordInfo);
-            }
-        }
-
-
-        return map;
-    }
-
-
 }
